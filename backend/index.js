187009c3345f38e97c791b26b95841879841a6b3
patinhere <patinhere@gmail.com>
2024-05-29 19:12:08 +1000

    cors access control allow origin

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ import multer from "multer";
 const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(express.json());
