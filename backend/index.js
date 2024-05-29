768e06787292ae0ae0d38ffa0af603762a82e937
patinhere <patinhere@gmail.com>
2024-05-29 21:36:38 +1000

    header

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.header("origin"));
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 app.use(express.json());
