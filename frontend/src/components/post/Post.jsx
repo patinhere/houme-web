e9a40ebb91b779b3a48ebbdcead04156dae5b202
patinhere <patinhere@gmail.com>
2024-06-15 19:04:48 +1000

    frontend sidebarbutton

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -117,7 +117,7 @@ const Post = ({ post }) => {
         </div>
         <div className="content">
           <p>{post.desc}</p>
-          <img src={"./upload/" + post.pic} alt="" />
+          <img src={post.pic ? "" : "./upload/" + post.pic} alt="" />
         </div>
         <div className="info">
           <div className="item">
